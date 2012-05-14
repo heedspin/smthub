200c5bc96a02a533c316f8950bc56f80c1242534
Tim Harrison <heedspin@gmail.com>
2012-05-14 09:18:06 -0400

    Disable obfuscator.

--- app/helpers/link_to_item_helper.rb
+++ app/helpers/link_to_item_helper.rb
@@ -1,6 +1,8 @@
 module LinkToItemHelper
   def link_to_item(item, part_number)
-    part_number = Obfuscator.instance.part_number(part_number)
+    if defined?(Obfuscator)
+      part_number = Obfuscator.instance.part_number(part_number)
+    end
     if item
       link_to_unless_current part_number, item_url(item)
     else
