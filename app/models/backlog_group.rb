56ec30007a905c366cefe587172373b0062c4678
Tim Harrison <heedspin@gmail.com>
2012-06-29 12:23:53 -0400

    Fix backlog group filter now that active hash methods uses cmethod instead of method.

--- app/models/backlog_group.rb
+++ app/models/backlog_group.rb
@@ -1,16 +1,16 @@
 require 'active_hash_methods'
 class BacklogGroup < ActiveHash::Base
   self.data = [
-    {:id => 1, :name => 'Not Paint', :method => 'not_paint'},
-    {:id => 2, :name => 'Paint Only', :method => 'only_paint'}
+    {:id => 1, :name => 'Not Paint', :cmethod => 'not_paint'},
+    {:id => 2, :name => 'Paint Only', :cmethod => 'only_paint'}
   ]
   include ActiveHashMethods
 
   def filter_release?(release)
     case self.cmethod
-    when 'not_paint'
+    when :not_paint
       release.part_number.present? && release.part_number.downcase.starts_with?('paint')
-    when 'only_paint'
+    when :only_paint
       !release.part_number.present? || !release.part_number.downcase.starts_with?('paint')
     else
       false
