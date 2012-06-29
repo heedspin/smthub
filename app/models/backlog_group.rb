db43e8e063391b9d95bb55c74a39d888b16754db
Tim Harrison <heedspin@gmail.com>
2012-06-29 13:15:25 -0400

    Change backlog filter to use item prod category.

--- app/models/backlog_group.rb
+++ app/models/backlog_group.rb
@@ -1,19 +1,13 @@
 require 'active_hash_methods'
 class BacklogGroup < ActiveHash::Base
   self.data = [
-    {:id => 1, :name => 'Not Paint', :cmethod => 'not_paint'},
-    {:id => 2, :name => 'Paint Only', :cmethod => 'only_paint'}
+    {:id => 1, :name => 'Paint Only', :product_class_keys => ['PO'] },
+    {:id => 2, :name => 'Contract', :product_class_keys => ['60'] },
+    {:id => 3, :name => 'Catalog', :product_class_keys => ['70'] }
   ]
   include ActiveHashMethods
 
   def filter_release?(release)
-    case self.cmethod
-    when :not_paint
-      release.part_number.present? && release.part_number.downcase.starts_with?('paint')
-    when :only_paint
-      !release.part_number.present? || !release.part_number.downcase.starts_with?('paint')
-    else
-      false
-    end
+    !self.product_class_keys.include?(release.item.item.product_class_key)
   end
 end
