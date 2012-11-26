590c9da9392dbe482cf0a7b1fb396992817a0778
Tim Harrison <heedspin@gmail.com>
2012-11-26 14:28:22 -0500

    Move active hash lib to pluto.

--- app/models/backlog_group.rb
+++ app/models/backlog_group.rb
@@ -1,11 +1,11 @@
-require 'active_hash_methods'
+require 'plutolib/active_hash_methods'
 class BacklogGroup < ActiveHash::Base
   self.data = [
     {:id => 1, :name => 'Paint Only', :product_class_keys => ['PO'] },
     {:id => 2, :name => 'Contract', :product_class_keys => ['60'] },
     {:id => 3, :name => 'Catalog', :product_class_keys => ['70'] }
   ]
-  include ActiveHashMethods
+  include Plutolib::ActiveHashMethods
 
   def filter_release?(release)
     !self.product_class_keys.include?(release.item.item.product_class_key)
